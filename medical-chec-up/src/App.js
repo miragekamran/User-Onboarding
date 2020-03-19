f98d9bb5425ff526621791d0ab5edb419dc83c76
mirage <mirage.kamran.pkt@gmail.com>
2020-03-19 00:03:42 -0700

    progress in making form using yup

--- medical-chec-up/src/App.js
+++ medical-chec-up/src/App.js
@@ -2,9 +2,11 @@ import React from 'react';
 import Form from './components/Form';
 import './App.css';
 
+
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{color: 'white'}}>
+      <h1>Medical Check-Up Form</h1>
       <Form />
     </div>
   );
