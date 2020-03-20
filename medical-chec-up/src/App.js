cdae4315472949c4977565d0b4f57e6fbc1672cf
mirage <mirage.kamran.pkt@gmail.com>
2020-03-19 18:30:59 -0700

    advance styles added to App.js

--- medical-chec-up/src/App.js
+++ medical-chec-up/src/App.js
@@ -5,7 +5,15 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App" style={{color: 'white'}}>
+    <div className="App" style={{
+              color: 'white',
+              border: '1px solid white',
+              display: 'flex',
+              flexDirection: 'column',
+              lexWrap: 'wrap',
+              textAlign: 'center',
+              minHeight: '80vh',
+              }}>
       <h1>Medical Check-Up Form</h1>
       <Form />
     </div>
--- medical-chec-up/src/components/Form.js
+++ medical-chec-up/src/components/Form.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios';
 import * as yup from 'yup';
+// import Reactstrap from 'reactstrap';
 
 const formSchema = yup.object().shape({
     fName: yup
